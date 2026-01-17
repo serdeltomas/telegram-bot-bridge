7636837d7c0271331644fa33a8ccb528d691cfe6
Tomáš Serdel <73882365+serdeltomas@users.noreply.github.com>
2026-01-17 14:14:09 +0100

    Update client.py

--- telethon_client/client.py
+++ telethon_client/client.py
@@ -1,9 +1,11 @@
+
 from telethon import TelegramClient
 from bot.config import API_ID, API_HASH
 
-client = TelegramClient('user_session', API_ID, API_HASH)
+
+client = TelegramClient("user_session", API_ID, API_HASH)
+
 
 async def start_client():
-    # user login
-    await client.start()  # will ask for phone + code once
-    print("✅ Telethon user client started")
+  if not client.is_connected():
+    await client.start()
