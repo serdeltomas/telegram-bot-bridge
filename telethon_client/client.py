af8ce65e65d332922fea4e5afddcb9ad392673b6
Tomáš Serdel <73882365+serdeltomas@users.noreply.github.com>
2026-01-17 12:57:15 +0100

    Create client.py

---
+++ telethon_client/client.py
@@ -0,0 +1,10 @@
+from telethon import TelegramClient
+from bot.config import API_ID, API_HASH
+
+
+client = TelegramClient("user_session", API_ID, API_HASH)
+
+
+async def start_client():
+if not client.is_connected():
+await client.start()
