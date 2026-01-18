8845b629474c0b41ded2c14aa6634bac45cf5941
Tomáš Serdel <73882365+serdeltomas@users.noreply.github.com>
2026-01-18 16:32:55 +0100

    Update main.py revert back

--- bot/main.py
+++ bot/main.py
@@ -3,16 +3,12 @@ from aiogram import Bot, Dispatcher
 from bot.config import BOT_TOKEN
 from bot.handlers import router
 from telethon_client.client import start_client
-from telethon_client.batch import process_csv
 
 async def main():
-    # Start Telethon client ONCE
+    # Start Telethon user client
     await start_client()
 
-    # 🔥 RUN CSV IMPORT HERE
-    await process_csv("songs.csv")
-
-    # Start Telegram bot AFTER batch finishes
+    # Start Telegram bot
     bot = Bot(token=BOT_TOKEN)
     dp = Dispatcher()
     dp.include_router(router)
